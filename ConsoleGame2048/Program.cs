1d845404e937670c3d2cd63bd18143fdd0881fd0
Потехин Сергей <ser.potehin2015@yandex.ru>
2020-03-24 22:17:34 +0300

    Init

---
+++ ConsoleGame2048/Program.cs
@@ -0,0 +1,53 @@
+﻿using Game2048;
+using System;
+
+namespace ConsoleGame2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+        void Start()
+        {
+            Model model = new Model(4);
+            model.Start();
+            while (true)
+            {
+                Show();
+                switch(Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.LeftArrow: model.Left(); break;
+                    case ConsoleKey.RightArrow: model.Right(); break;
+                    case ConsoleKey.UpArrow: model.Up(); break;
+                    case ConsoleKey.DownArrow: model.Down(); break;
+                    case ConsoleKey.Escape: model.Start(); break;
+
+                }
+            }
+        }
+
+        private void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Show(Model model)
+        {
+            for (int y = 0; y < model.size; y++)
+                for (int x = 0; x < model.size; x++)
+                {
+                    Console.SetCursorPosition(x * 5 + 5, y * 2 + 2);
+                    int number = model.GetMap(x, y);
+                    Console.WriteLine(number == 0 ? " " : number.ToString());
+                }
+            Console.WriteLine();
+            if (model.isGameOver)
+                Console.WriteLine("Game Over");
+            else
+                Console.WriteLine("Still play");
+         }
+    }
+}
---
+++ Game2048/Model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2048
+{
+    public class Model
+    {
+        bool isGameOver;
+        public int size { get; private set; }
+        public bool IsGameOver { get; set; }
+
+        public Model(int size)
+        {
+            this.size = size;
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Left()
+        {
+        }
+
+        public void Right()
+        {
+        }
+
+        public void Up()
+        {
+        }
+
+        public void Down()
+        {
+        }
+
+        public int GetMap(int x, int y)
+        {
+            return 0;
+        }
+        public bool IsGameOver()
+        {
+            return IsGameOver;
+        }
+    }
+}
